a91cf90c9eebe2940de7860101ed4c77031076c2
Viktorja Bezsazna <viktorija.bezsazna@gmail.com>
2017-09-18 12:04:49 -0400

    Move 'get available times' query from controller to model

--- app/controllers/family_profile_controller.rb
+++ app/controllers/family_profile_controller.rb
@@ -1,7 +1,6 @@
 class FamilyProfileController < ApplicationController
   def index
-  	@available_babysitting_times = SelectedBabysittingTime.where('family_id': nil).joins(:babysitter, :babysitting_time, :babysitting_day)
-  									.select('id', 'family_id', 'name', 'babysitting_day_id', 'day', 'babysitting_time_id', 'start_time', 'end_time')
+  	@available_babysitting_times = SelectedBabysittingTime.get_available_babysitting_times
   end
 
    def create
